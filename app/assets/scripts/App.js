d3700cc3a255cdc6a778807205bcdc49dcd53718
dgentilli <dgentilli@gmail.com>
2020-08-19 15:54:26 +0300

    Small fix for Netlify deploy.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -14,7 +14,7 @@ document.querySelectorAll(".open-modal").forEach((el) =>
   el.addEventListener("click", (e) => {
     e.preventDefault();
     if (typeof modal == "undefined") {
-      import(/*webpackChunkName: "modal" */ "./modules/modal")
+      import(/*webpackChunkName: "modal" */ "./modules/Modal")
         .then((x) => {
           modal = new x.default();
           setTimeout(() => modal.openTheModal(), 20);
